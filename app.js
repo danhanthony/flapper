ce5f257ca91b40f5cf155e0339f938dab23f67e0
danhanthony <adanh@mail.sfsu.edu>
2016-02-29 07:21:57 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var app = angular.module('flapperNews', ['ui.router']);
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+
+app.controller('MainCtrl', [
+'$scope','posts',
+function($scope, posts){
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
+  $scope.title = '';
+  $scope.link = '';
+};
+
+
+$scope.incrementUpvotes = function(post) {
+  post.upvotes += 1;
+};
+
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('posts', {
+  url: '/posts/{id}',
+  templateUrl: '/posts.html',
+  controller: 'PostsCtrl'
+});
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+
+}]);
